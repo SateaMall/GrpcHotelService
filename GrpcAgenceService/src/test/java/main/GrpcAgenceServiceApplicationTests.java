0e23f7a44dcec56fdbfa57f9872f9372f307a387
Satea <mohamad-satea.almallouhi@etu.umontpellier.fr>
2023-12-18 18:50:58 +0100

    preparing the enviroment

---
+++ GrpcAgenceService/src/test/java/main/GrpcAgenceServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan(basePackages = {"models"})
+@EnableJpaRepositories(basePackages = {"repositories"})
+@SpringBootApplication(scanBasePackages = {"data","exceptions","controllers","Client"})
+class GrpcAgenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- HotelGRPC/src/main/java/controllers/GrpcServerService.java
+++ HotelGRPC/src/main/java/controllers/GrpcServerService.java
@@ -7,7 +7,6 @@ import net.devh.boot.grpc.server.service.GrpcService;
 @GrpcService
 public class GrpcServerService {
 
-
     public void sayHello(Protobuf.HelloRequest req, StreamObserver<Protobuf.HelloReply> responseObserver) {
         Protobuf.HelloReply reply = Protobuf.HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
         responseObserver.onNext(reply);
