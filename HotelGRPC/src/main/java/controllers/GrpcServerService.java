8f15536a2bff152ac741042d7054773f3133e11c
Satea <mohamad-satea.almallouhi@etu.umontpellier.fr>
2023-12-18 17:32:46 +0100

    adding new project agence

---
+++ HotelGRPC/src/main/java/controllers/GrpcServerService.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import helloworld.Protobuf;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GrpcServerService {
+
+
+    public void sayHello(Protobuf.HelloRequest req, StreamObserver<Protobuf.HelloReply> responseObserver) {
+        Protobuf.HelloReply reply = Protobuf.HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
