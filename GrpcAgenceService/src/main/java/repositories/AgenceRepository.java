71462056b72df3b4d21864567cf672effd6a2161
Satea <mohamad-satea.almallouhi@etu.umontpellier.fr>
2023-12-20 20:32:09 +0100

    k

--- GrpcAgenceService/src/main/java/repositories/AgenceRepository.java
+++
@@ -1,7 +0,0 @@
-package repositories;
-
-import models.Agence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgenceRepository extends JpaRepository<Agence,Long> {
-}
--- HotelGRPC/src/main/java/repositories/AdresseRepository.java
+++
@@ -1,7 +0,0 @@
-package repositories;
-
-import models.Adresse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdresseRepository extends JpaRepository<Adresse,Long> {
-}
